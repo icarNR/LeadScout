cd3d5099f187329519e87afaf42406cec9198e2b
Bhawa1018 <124970079+Bhawa1018@users.noreply.github.com>
2024-07-02 21:54:48 +0530

    Update main.py

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,3 +1,13 @@
+from fastapi import FastAPI
+from routes.Employee_Criteria import router as employee_criteria_router
+from crorSetting import setup_cors
+
+app = FastAPI()
+
+setup_cors(app)
+
+# Include the router from Employee_Criteria.py
+app.include_router(employee_criteria_router)
 
 
 # The rest of your FastAPI application code goes here
